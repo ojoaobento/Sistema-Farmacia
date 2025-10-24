3cbf63655055f166415333f9a484aea0fee71e4c
ojoaobento <joaovictorbento.s.b@gmail.com>
2025-10-24 14:08:16 -0300

    Desenvolvendo logica do inclusaoCliente

--- inclusaoCliente.c
+++ inclusaoCliente.c
@@ -3,42 +3,73 @@
 void inclusaoCliente(TipoListaCliente *lista){
     
     ApontadorCliente novoCadastro;
+    char confirmacao;
+    char inclusao;
 
-    novoCadastro = (ApontadorCliente) malloc(sizeof(TipoCliente));
+    do {
+        novoCadastro = (ApontadorCliente) malloc(sizeof(TipoCliente));
 
-    system("cls");
-    tela();
+        system("cls");
+        tela();
 
-    telaCliente();
+        telaCliente();
 
-    gotoxy(11,10);
-    scanf("%d", &novoCadastro->conteudo.id_cliente);
+        gotoxy(11,10);
+        scanf("%d", &novoCadastro->conteudo.id_cliente);
 
-    gotoxy(31,10);
-    fflush(stdin);
-    fgets(novoCadastro->conteudo.nome,50,stdin);
+        gotoxy(31,10);
+        fflush(stdin);
+        fgets(novoCadastro->conteudo.nome,50,stdin);
 
-    gotoxy(12,12);
-    fflush(stdin);
-    fgets(novoCadastro->conteudo.cpf,15,stdin);
+        gotoxy(12,12);
+        fflush(stdin);
+        fgets(novoCadastro->conteudo.cpf,15,stdin);
 
-    gotoxy(55,12);
-    fflush(stdin);
-    fgets(novoCadastro->conteudo.telefone,20,stdin);
+        gotoxy(55,12);
+        fflush(stdin);
+        fgets(novoCadastro->conteudo.telefone,20,stdin);
 
-    gotoxy(14,14);
-    fflush(stdin);
-    fgets(novoCadastro->conteudo.email,50,stdin);
+        gotoxy(14,14);
+        fflush(stdin);
+        fgets(novoCadastro->conteudo.email,50,stdin);
 
-    gotoxy(17,16);
-    fflush(stdin);
-    fgets(novoCadastro->conteudo.endereco,60,stdin);
+        gotoxy(17,16);
+        fflush(stdin);
+        fgets(novoCadastro->conteudo.endereco,60,stdin);
 
-    gotoxy(22,18);
-    fflush(stdin);
-    fgets(novoCadastro->conteudo.data_cadastro,19,stdin);
+        gotoxy(22,18);
+        fflush(stdin);
+        fgets(novoCadastro->conteudo.data_cadastro,19,stdin);
 
-    gotoxy(53,18);
-    scanf("%d", &novoCadastro->conteudo.status);
+        gotoxy(53,18);
+        scanf("%d", &novoCadastro->conteudo.status);
+
+
+        gotoxy(30,19);
+        printf("CONFIRMA INCLUSAO ( S/N ) ? ");
+        scanf(" %c", &inclusao);
+
+        if(confirmacao == 's' || confirmacao == 'S'){
+
+            if(lista->primeiro == NULL){
+                lista->primeiro = novoCadastro;
+                lista->ultimo = novoCadastro;
+                novoCadastro->proximo = NULL;
+            } else {
+                lista->ultimo->proximo = novoCadastro;
+                lista->ultimo = novoCadastro;
+                novoCadastro->proximo = NULL;
+            }
+
+        } else {
+            return;
+        }
+
+        gotoxy(30,20);
+        printf("NOVA INCLUSAO ( S/N ) ? ");
+        scanf(" %c", &inclusao);
+
+
+    }while(inclusao != 'n' && inclusao != 'N');
 
 }
