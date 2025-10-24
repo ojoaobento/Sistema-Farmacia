478e025ef79bfd5ddb84586ad431f1165105050f
ojoaobento <joaovictorbento.s.b@gmail.com>
2025-10-24 13:16:32 -0300

    Adicionando telaCliente e Desenvolvendo o inclusaoCliente

---
+++ inclusaoCliente.c
@@ -0,0 +1,17 @@
+#include "funcoes.h"
+
+void inclusaoCliente(TipoListaCliente *lista){
+    
+    ApontadorCliente novoCadastro;
+
+    novoCadastro = (ApontadorCliente) malloc(sizeof(TipoCliente));
+
+
+    
+    
+    system("cls");
+    tela();
+
+
+
+}
---
+++ telaCliente.c
@@ -0,0 +1,39 @@
+#include "funcoes.h"
+
+void telaCliente(){
+
+    // int id_cliente;
+    // char nome[50];
+    // char cpf[15];
+    // char telefone[20];
+    // char email[50];
+    // char endereco[80];
+    // char data_cadastro[19];
+    // int status;
+
+    gotoxy(4,5);
+    printf("ID: ");
+
+    gotoxy(4,20);
+    printf("NOME: ");
+
+    gotoxy(4,6);
+    printf("CPF: ");
+
+    gotoxy(4,22);
+    printf("TELEFONE: ");
+
+    gotoxy(4,7);
+    printf("EMAIL: ");
+
+    printf(4,8);
+    printf("ENDERECO: ");
+
+    gotoxy(4,9);
+    printf("DATA CADASTRO: ");
+
+    gotoxy(4,32);
+    printf("STATUS: ");
+
+
+}
