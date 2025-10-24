c75cd891edd04d2b9c5641808351cf9db24c7931
ojoaobento <joaovictorbento.s.b@gmail.com>
2025-10-24 13:43:37 -0300

    Inserindo entrada de dados em inclusaoCliente

--- inclusaoCliente.c
+++ inclusaoCliente.c
@@ -6,16 +6,39 @@ void inclusaoCliente(TipoListaCliente *lista){
 
     novoCadastro = (ApontadorCliente) malloc(sizeof(TipoCliente));
 
-
-
-    
     system("cls");
     tela();
 
     telaCliente();
 
-    getch();
+    gotoxy(9,10);
+    scanf("%d", &novoCadastro->conteudo.id_cliente);
 
+    gotoxy(50,10);
+    fflush(stdin);
+    fgets(novoCadastro->conteudo.nome,50,stdin);
 
+    gotoxy(15,12);
+    fflush(stdin);
+    fgets(novoCadastro->conteudo.cpf,15,stdin);
+
+    gotoxy(55,12);
+    fflush(stdin);
+    fgets(novoCadastro->conteudo.telefone,20,stdin);
+
+    gotoxy(15,14);
+    fflush(stdin);
+    fgets(novoCadastro->conteudo.email,50,stdin);
+
+    gotoxy(17,16);
+    fflush(stdin);
+    fgets(novoCadastro->conteudo.endereco,80,stdin);
+
+    gotoxy(17,18);
+    fflush(stdin);
+    fgets(novoCadastro->conteudo.data_cadastro,19,stdin);
+
+    gotoxy(55,18);
+    scanf("%d", &novoCadastro->conteudo.status);
 
 }
