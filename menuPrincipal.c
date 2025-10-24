f2d54fa73828d9012ac9a5855af47e73e21f3ee7
ojoaobento <joaovictorbento.s.b@gmail.com>
2025-10-24 13:05:07 -0300

    Atualizando logica do menuPrincipal

--- menuPrincipal.c
+++ menuPrincipal.c
@@ -4,50 +4,52 @@ void menuPrincipal(){
     
     int opcao;
 
-    gotoxy(35,12);
-    printf("1- CLIENTES");
+    do {
+        gotoxy(35,12);
+        printf("1- CLIENTES");
 
-    gotoxy(35,13);
-    printf("2- MEDICAMENTOS");
+        gotoxy(35,13);
+        printf("2- MEDICAMENTOS");
 
-    gotoxy(35,14);
-    printf("3- FORNECEDORES");
+        gotoxy(35,14);
+        printf("3- FORNECEDORES");
 
-    gotoxy(35,15);
-    printf("4- MOVIMENTACOES");
+        gotoxy(35,15);
+        printf("4- MOVIMENTACOES");
 
-    gotoxy(35,16);
-    printf("5- RELATORIOS");
+        gotoxy(35,16);
+        printf("5- RELATORIOS");
 
-    gotoxy(35,17);
-    printf("6- SAIR");
+        gotoxy(35,17);
+        printf("6- SAIR");
 
-    gotoxy(35,19);
-    printf("OPCAO: ");
-    scanf("%d", &opcao);
+        gotoxy(35,19);
+        printf("OPCAO: ");
+        scanf("%d", &opcao);
 
-    switch(opcao){
-        case 1:
-            menuClientes();
-            break;
-        case 2:
+        switch(opcao){
+            case 1:
+                menuClientes();
+                break;
+            case 2:
 
-            break;
-        case 3:
+                break;
+            case 3:
 
-            break;
-        case 4:
+                break;
+            case 4:
 
-            break;
-        case 5:
+                break;
+            case 5:
 
-            break;
-        case 6:
+                break;
+            case 6:
 
-            break;
-        default:
+                break;
+            default:
 
-            break;
-    }
+                break;
+        }
 
+    }while(opcao != 6);
 }
