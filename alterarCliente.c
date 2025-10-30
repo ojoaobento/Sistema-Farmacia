23dd8593da3079ebab22a71794f80a9b9e7f9eab
João Bento <joaovictorbento.s.b@gmail.com>
2025-10-29 21:36:14 -0300

    Desenvolvendo metódo de switch case em Alterar Cliente

--- alterarCliente.c
+++ alterarCliente.c
@@ -4,7 +4,11 @@ void alterarCliente(TipoListaCliente *lista){
 
     int codigo;
     int opcao;
+    int codigoVerificacao;
+    int alteracao;
+    reg_clientes temporaria;
     ApontadorCliente resultado;
+    ApontadorCliente p_auxiliar;
 
     system("cls");
     tela();
@@ -36,7 +40,6 @@ void alterarCliente(TipoListaCliente *lista){
         gotoxy(11,9);
         printf("%d", resultado->conteudo.id_cliente);
 
-
         gotoxy(31,9);
         printf("%s", resultado->conteudo.nome);
 
@@ -60,13 +63,68 @@ void alterarCliente(TipoListaCliente *lista){
 
         limpa_msg();
         gotoxy(2,23);
-        printf("ALTERAR CAMPO(1-NOME,2-CPF,3-TEL,4-EMAIL,5-END,6-DATA,7-STATUS): ");
+        printf("ALTERAR CAMPO(1-ID,2-NOME,3-CPF,4-TEL,5-EMAIL,6-END,7-DATA,8-STATUS,0-SAIR): ");
         scanf("%d", &opcao);
 
+        switch(opcao){
+            case 1:
+
+                do {
+                    codigoVerificacao = 0;
+                    limpa_msg();
+                    gotoxy(11,9);
+                    scanf("%d", temporaria.id_cliente);
+
+                    p_auxiliar = lista->primeiro;
+
+                    while(p_auxiliar != NULL){
+                        if(p_auxiliar->conteudo.id_cliente == temporaria.id_cliente){
+
+                            limpa_msg();
+                            gotoxy(2,23);
+                            printf("O CODIGO INSERIDO JA EXISTE, INSIRA OUTRO........");
+                            codigoVerificacao = 1;
+                            getch();
+
+                            break;
+                        }
+
+                        p_auxiliar = p_auxiliar->proximo;
+                    }
+                } while(codigoVerificacao == 1);
+
+
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 0:
+
+                break;
+            default:
+
+                break;
+        }
 
     }
-
-
-
 
 }
