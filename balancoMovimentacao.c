6481d5c477e166bc523569745b1c9586972860b9
João Bento <joaovictorbento.s.b@gmail.com>
2026-02-09 10:33:20 -0300

    Concluindo paginação de balancoMovimentação

--- balancoMovimentacao.c
+++ balancoMovimentacao.c
@@ -15,121 +15,156 @@ void balancoMovimentacao(TipoListaCliente *listaCliente,TipoListaMedicamento *li
     int cont=0;
     int verificacao;
     int quantidade_vendida;
+    int paginaAtual=0;
+    char alternativa;
 
-    
-    system("cls");
-    telaBalancoMovimentacao();
+    do {
+        if(paginaAtual == 0){
+            system("cls");
+            telaBalancoMovimentacao();
 
-    r = listaCliente->primeiro;
-    p = listaMedicamento->primeiro;
+            contC = 0;
+            contM = 0;
+            contV = 0;
+            contSV = 0;
 
-    while(r != NULL){
-        contC++;
-        r = r->proximo;
-    }
+            r = listaCliente->primeiro;
+            p = listaMedicamento->primeiro;
 
-    while(p != NULL){
-        contM++;
-        p = p->proximo;
-    }
+            paginaAtual=0;
 
-    p = listaMedicamento->primeiro;
-
-    while(p != NULL){
-        t = listaMovimentacao->primeiro;
-        verificacao=0;
-        while(t != NULL){
-            contV += t->conteudo.quantidade;
-            if(t->conteudo.cd_medicamento == p->conteudo.id){
-                verificacao = 1;
+            while(r != NULL){
+                contC++;
+                r = r->proximo;
             }
-            t = t->proximo;
-        }
-        if(verificacao == 0){
-            contSV++;
-        }
-        p = p->proximo;
-    }
 
-    gotoxy(42,11);
-    printf("%d", contC);
-
-    gotoxy(42,12);
-    printf("%d", contM);
-
-    gotoxy(42,13);
-    printf("%d", contV);
-
-    gotoxy(42,14);
-    printf("%d", contSV);
-
-
-    limpa_msg();
-    gotoxy(2,23);
-    printf("SEGUIR PARA PROXIMA PAGINA....");
-    getch();
-    system("cls");
-    telaRanking();
-
-    p = listaMedicamento->primeiro;
-    while(p != NULL){
-        t = listaMovimentacao->primeiro;
-        quantidade_vendida=0;
-
-        while(t != NULL){
-            if(t->conteudo.cd_medicamento == p->conteudo.id){
-                quantidade_vendida += t->conteudo.quantidade;
+            while(p != NULL){
+                contM++;
+                p = p->proximo;
             }
-            t = t->proximo;
-        }
 
-        if(quantidade_vendida > 0){
-            novo = (ApontadorRanking) malloc(sizeof(TipoRanking));
-            novo->conteudo.id = p->conteudo.id;
-            strcpy(novo->conteudo.nome_medicamento, p->conteudo.nome);
-            novo->conteudo.quantidade_vendida = quantidade_vendida;
+            p = listaMedicamento->primeiro;
 
-            if(listaRanking->primeiro == NULL){
-                listaRanking->primeiro = novo;
-                listaRanking->ultimo = novo;
-                listaRanking->ultimo->proximo = NULL;
-            }else{
-                listaRanking->ultimo->proximo = novo;
-                listaRanking->ultimo = novo;
-                listaRanking->ultimo->proximo = NULL;
+            while(p != NULL){
+                t = listaMovimentacao->primeiro;
+                verificacao=0;
+                while(t != NULL){
+                    contV += t->conteudo.quantidade;
+                    if(t->conteudo.cd_medicamento == p->conteudo.id){
+                        verificacao = 1;
+                    }
+                    t = t->proximo;
+                }
+                if(verificacao == 0){
+                    contSV++;
+                }
+                p = p->proximo;
             }
+
+            gotoxy(42,11);
+            printf("%d", contC);
+
+            gotoxy(42,12);
+            printf("%d", contM);
+
+            gotoxy(42,13);
+            printf("%d", contV);
+
+            gotoxy(42,14);
+            printf("%d", contSV);
+        }else if (paginaAtual == 1){
+            system("cls");
+            telaRanking();
+            paginaAtual=1;
+            p = listaMedicamento->primeiro;
+            while(p != NULL){
+                t = listaMovimentacao->primeiro;
+                quantidade_vendida=0;
+
+                while(t != NULL){
+                    if(t->conteudo.cd_medicamento == p->conteudo.id){
+                        quantidade_vendida += t->conteudo.quantidade;
+                    }
+                    t = t->proximo;
+                }
+
+                if(quantidade_vendida > 0){
+                    novo = (ApontadorRanking) malloc(sizeof(TipoRanking));
+                    novo->conteudo.id = p->conteudo.id;
+                    strcpy(novo->conteudo.nome_medicamento, p->conteudo.nome);
+                    novo->conteudo.quantidade_vendida = quantidade_vendida;
+
+                    if(listaRanking->primeiro == NULL){
+                        listaRanking->primeiro = novo;
+                        listaRanking->ultimo = novo;
+                        listaRanking->ultimo->proximo = NULL;
+                    }else{
+                        listaRanking->ultimo->proximo = novo;
+                        listaRanking->ultimo = novo;
+                        listaRanking->ultimo->proximo = NULL;
+                    }
+                }
+
+                p = p->proximo;
+            }
+
+            ordenarRanking(listaRanking);
+
+            
+            i=11;
+            h = listaRanking->primeiro;
+            cont=0;
+            while(h != NULL && cont<=10){
+
+                gotoxy(3,i);
+                printf("%d", h->conteudo.id);
+
+                gotoxy(32,i);
+                printf("%s", h->conteudo.nome_medicamento);
+
+                gotoxy(67,i);
+                printf("%d", h->conteudo.quantidade_vendida);
+
+                cont++;
+                i++;
+                h = h->proximo;
+            }
+
+            esvaziarRanking(listaRanking);
         }
 
-        p = p->proximo;
-    }
+        limpa_msg();
+        gotoxy(2,23);
+        printf("[P] PROXIMA - [A] ANTERIOR - [S] SAIR : ");
+        scanf(" %c", &alternativa);
 
-    ordenarRanking(listaRanking);
+        switch(alternativa){
+            case 'P':
+            case 'p':
+                paginaAtual++;
+                if(paginaAtual > 1){
+                    paginaAtual--;
+                }
+                break;
+            case 'A':
+            case 'a':
+                paginaAtual--;
+                if(paginaAtual < 0){
+                    paginaAtual++;
+                }
+                break;
+            case 'S':
+            case 's':
+                return;
+                break;
+            default:
+                limpa_msg();
+                gotoxy(2,23);
+                printf("OPCAO INVALIDA, INSIRA NOVAMENTE......");
+                getch();
+                break;
+        }
 
-    
-    i=11;
-    h = listaRanking->primeiro;
 
-    while(h != NULL && cont<=10){
-        gotoxy(3,i);
-        printf("%d", h->conteudo.id);
-
-        gotoxy(32,i);
-        printf("%s", h->conteudo.nome_medicamento);
-
-        gotoxy(67,i);
-        printf("%d", h->conteudo.quantidade_vendida);
-
-        cont++;
-        i++;
-        h = h->proximo;
-    }
-
-    esvaziarRanking(listaRanking);
-
-    limpa_msg();
-    gotoxy(2,23);
-    printf("PRESSIONE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL...");
-
-    getch();
-    return;
+    }while(alternativa != 's' && alternativa != 'S');
 }
